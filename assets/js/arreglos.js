2b760e0078ee491c5aa106f0c02afe7e5ed6e3ce
JeffersonCabello <jefcsanc@espol.edu.ec>
2024-10-07 20:06:54 -0500

    arreglos

---
+++ assets/js/arreglos.js
@@ -0,0 +1,31 @@
+//const arr = new Array(10);
+
+//console.log(arr);
+
+let videoJuegos = ['Mario 3','Megaman','Chrono Trigger'];
+console.log({videoJuegos});
+
+console.log(videoJuegos[0]);
+
+let arregloCosas = [
+    true,
+    123,
+    'Jeff',
+    1+2,
+    function(){
+
+    },
+    () =>{},
+    {a:1},
+    ['X','Megaman','Zero','Dr. Light',[
+        'Dr. Willy',
+        'Woodman'
+    ]]
+];
+
+console.log({arregloCosas});
+
+console.log(arregloCosas[0]);
+
+console.log(arregloCosas[7][4][1]);
+
