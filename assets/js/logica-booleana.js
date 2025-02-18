12e251e52f8bfc60d8539bb74294490a41c93406
JeffersonCabello <jefcsanc@espol.edu.ec>
2025-02-18 07:31:26 -0500

    asignaciones con operadores

--- assets/js/logica-booleana.js
+++ assets/js/logica-booleana.js
@@ -1,5 +1,3 @@
-
-
 const regresaTrue = () =>{
     console.log("Regresa true");
     return true;
@@ -15,7 +13,6 @@ console.log(true);
 
 console.log(!true);
 console.log(!false);
-
 
 console.log(!regresaFalse());
 
@@ -40,5 +37,22 @@ console.log(false || false); //false
 console.log(regresaTrue() || regresaFalse());
 console.log("4 condiciones", true || true || true || false); //true
 
-
 console.warn("Asignaciones");
+
+const soyUndefined = undefined;
+const soyNull = null;
+const soyFalso = false;
+
+const a1 = false && "Hola mundo" && 150;
+const a2 = "Hola" && "Mundo" && soyFalso && true;
+const a3 = soyFalso || "Ya no soy falso";
+const a4 = soyFalso || soyUndefined || soyNull || "Ya no soy falso de nuevo" || true;
+const a5 = soyFalso || soyUndefined || regresaTrue() || "Ya no soy falso de nuevo" || true;
+
+console.log({a1,a2,a3, a4,a5})
+
+if(regresaFalse() && regresaTrue() && (true || true || true || false)){
+    console.log("Hacer algo");
+}else{
+    console.log("Hacer otra cosa");
+}
